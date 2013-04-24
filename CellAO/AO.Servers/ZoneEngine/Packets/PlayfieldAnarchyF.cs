825a16596c4492eb2da048e2fcf9cf6a5e2bb6b2
bitterskittles <basementk1tt3h@gmail.com>
2013-04-24 13:54:59 +0100

    PlayfieldAnarchyF now constructs PlayfieldAnarchyFMessage.

--- CellAO/AO.Servers/ZoneEngine/Packets/PlayfieldAnarchyF.cs
+++ CellAO/AO.Servers/ZoneEngine/Packets/PlayfieldAnarchyF.cs
@@ -1,84 +1,93 @@
-﻿#region License
-// Copyright (c) 2005-2012, CellAO Team
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
-//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayfieldAnarchyF.cs" company="CellAO Team">
+//   Copyright © 2005-2013 CellAO Team.
+//   
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//   
+//       * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//       * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//       * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+// <summary>
+//   Defines the PlayfieldAnarchyF type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ZoneEngine.Packets
 {
-    using AO.Core;
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
 
     using ZoneEngine.Misc;
     using ZoneEngine.NonPlayerCharacter;
 
-    /// <summary>
-    /// 
-    /// </summary>
     public static class PlayfieldAnarchyF
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="client"></param>
+        #region Public Methods and Operators
+
         public static void Send(Client client)
         {
-            PacketWriter packetWriter = new PacketWriter();
+            var message = new PlayfieldAnarchyFMessage
+                              {
+                                  Identity =
+                                      new Identity
+                                          {
+                                              IdentityType = IdentityType.Playfield2, 
+                                              Instance = client.Character.PlayField
+                                          }, 
+                                  CharacterCoordinates =
+                                      new Vector3
+                                          {
+                                              X = client.Character.Coordinates.x, 
+                                              Y = client.Character.Coordinates.y, 
+                                              Z = client.Character.Coordinates.z, 
+                                          }, 
+                                  PlayfieldId1 =
+                                      new Identity
+                                          {
+                                              IdentityType = IdentityType.Playfield1, 
+                                              Instance = client.Character.PlayField
+                                          }, 
+                                  PlayfieldId2 =
+                                      new Identity
+                                          {
+                                              IdentityType = IdentityType.Playfield2, 
+                                              Instance = client.Character.PlayField
+                                          }, 
+                                  PlayfieldX =
+                                      Playfields.GetPlayfieldX(client.Character.PlayField), 
+                                  PlayfieldZ =
+                                      Playfields.GetPlayfieldZ(client.Character.PlayField)
+                              };
 
-            packetWriter.PushBytes(new byte[] { 0xDF, 0xDF });
-            packetWriter.PushShort(10);
-            packetWriter.PushShort(1);
-            packetWriter.PushShort(0);
-            packetWriter.PushInt(3086);
-            packetWriter.PushInt(client.Character.Id);
-            packetWriter.PushInt(0x5F4B1A39);
-            packetWriter.PushIdentity(40016, client.Character.PlayField);
-            packetWriter.PushByte(0);
-
-            packetWriter.PushInt(4);
-            packetWriter.PushCoord(client.Character.Coordinates);
-            packetWriter.PushByte(97);
-            packetWriter.PushIdentity(51100, client.Character.PlayField);
-            packetWriter.PushInt(0);
-            packetWriter.PushInt(0);
-            packetWriter.PushIdentity(40016, client.Character.PlayField);
-            packetWriter.PushInt(0);
-            packetWriter.PushInt(0);
-
-            int vendorcount = VendorHandler.GetNumberofVendorsinPlayfield(client.Character.PlayField);
+            var vendorcount = VendorHandler.GetNumberofVendorsinPlayfield(client.Character.PlayField);
             if (vendorcount > 0)
             {
-                packetWriter.PushInt(51035);
-                packetWriter.PushInt(1);
-                packetWriter.PushInt(1);
-                packetWriter.PushInt(vendorcount);
-                packetWriter.PushInt(VendorHandler.GetFirstVendor(client.Character.PlayField));
+                var firstVendorId = VendorHandler.GetFirstVendor(client.Character.PlayField);
+                message.PlayfieldVendorInfo = new PlayfieldVendorInfo
+                                                  {
+                                                      VendorCount = vendorcount, 
+                                                      FirstVendorId = firstVendorId
+                                                  };
             }
-            // TODO: Use correct World Position for each "outdoors" playfield -Suiv-
-            // Playfield WorldPos X
-            packetWriter.PushInt(Playfields.GetPlayfieldX(client.Character.PlayField));
-            // Playfield WorldPos Z
-            packetWriter.PushInt(Playfields.GetPlayfieldZ(client.Character.PlayField));
 
-            byte[] packet = packetWriter.Finish();
-            client.SendCompressed(packet);
+            client.SendCompressed(3086, client.Character.Id, message);
         }
+
+        #endregion
     }
 }
