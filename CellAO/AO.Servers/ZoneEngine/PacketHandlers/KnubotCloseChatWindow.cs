4980454df3af1210b3ba95614886baa13c52f23d
bitterskittles <basementk1tt3h@gmail.com>
2013-04-27 16:18:05 +0100

    KnuBotCloseChatWindow now constructs KnuBotCloseChatWindowMessage.

--- CellAO/AO.Servers/ZoneEngine/PacketHandlers/KnubotCloseChatWindow.cs
+++ CellAO/AO.Servers/ZoneEngine/PacketHandlers/KnubotCloseChatWindow.cs
@@ -1,49 +1,58 @@
-﻿#region License
-// Copyright (c) 2005-2012, CellAO Team
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
-//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
-
-#region Usings...
-#endregion
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KnubotCloseChatWindow.cs" company="CellAO Team">
+//   Copyright © 2005-2013 CellAO Team.
+//   
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//   
+//       * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//       * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//       * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+// <summary>
+//   Defines the KnuBotCloseChatWindow type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ZoneEngine.PacketHandlers
 {
     using AO.Core;
 
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
+
     using ZoneEngine.Misc;
+
+    using Identity = SmokeLounge.AOtomation.Messaging.GameData.Identity;
 
     public static class KnuBotCloseChatWindow
     {
+        #region Public Methods and Operators
+
         public static void Read(byte[] packet, Client client)
         {
-            PacketReader packetReader = new PacketReader(packet);
+            var packetReader = new PacketReader(packet);
 
-            Header header = packetReader.PopHeader();
+            var header = packetReader.PopHeader();
             packetReader.PopByte();
             packetReader.PopShort();
-            int type = packetReader.PopInt();
-            int instance = packetReader.PopInt();
-            NonPlayerCharacterClass npc = (NonPlayerCharacterClass)FindDynel.FindDynelById(type, instance);
-            Character ch = FindClient.FindClientById(header.Sender).Character;
+            var type = packetReader.PopInt();
+            var instance = packetReader.PopInt();
+            var npc = (NonPlayerCharacterClass)FindDynel.FindDynelById(type, instance);
+            var ch = FindClient.FindClientById(header.Sender).Character;
             if (npc != null)
             {
                 npc.KnuBotCloseChatWindow(client.Character);
@@ -52,30 +61,32 @@ namespace ZoneEngine.PacketHandlers
 
         public static void Send(Character talkingTo, NonPlayerCharacterClass talker)
         {
-            Client client = talkingTo.Client;
-
-            PacketWriter pw = new PacketWriter();
-
-            pw.PushByte(0xdf);
-            pw.PushByte(0xdf);
-            pw.PushShort(10);
-            pw.PushShort(1);
-            pw.PushShort(0);
-            pw.PushInt(3086);
-            pw.PushInt(client.Character.Id);
-            pw.PushInt(0x270a4c62);
-            pw.PushIdentity(50000, talkingTo.Id);
-            pw.PushByte(0);
-            pw.PushShort(2);
-            pw.PushIdentity(50000, talker.Id);
-            pw.PushInt(5);
-            pw.PushInt(0);
-
-            byte[] packet = pw.Finish();
-            client.SendCompressed(packet);
+            var client = talkingTo.Client;
+            var message = new KnuBotCloseChatWindowMessage
+                              {
+                                  Identity =
+                                      new Identity
+                                          {
+                                              Type = IdentityType.CanbeAffected, 
+                                              Instance = talkingTo.Id
+                                          }, 
+                                  Unknown = 0x00, 
+                                  Unknown1 = 2, 
+                                  Target =
+                                      new Identity
+                                          {
+                                              Type = IdentityType.CanbeAffected, 
+                                              Instance = talker.Id
+                                          }, 
+                                  Unknown2 = 5, 
+                                  Unknown3 = 0
+                              };
+            client.SendCompressed(message);
 
             // Closing KnuBot window from server side needs to clear KnuBot's variables
             talker.KnuBot.KnuBotCloseChatWindow(talkingTo);
         }
+
+        #endregion
     }
 }
