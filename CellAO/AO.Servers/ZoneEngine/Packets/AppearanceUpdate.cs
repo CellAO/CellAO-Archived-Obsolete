fc226c406731f1f176a975066a20d0f821b59ab9
bitterskittles <basementk1tt3h@gmail.com>
2013-04-27 18:30:26 +0100

    AppearanceUpdate now constructs AppearanceUpdateMessage.

--- CellAO/AO.Servers/ZoneEngine/Packets/AppearanceUpdate.cs
+++ CellAO/AO.Servers/ZoneEngine/Packets/AppearanceUpdate.cs
@@ -1,56 +1,69 @@
-﻿#region License
-// Copyright (c) 2005-2012, CellAO Team
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
-//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppearanceUpdate.cs" company="CellAO Team">
+//   Copyright © 2005-2013 CellAO Team.
+//   
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//   
+//       * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//       * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//       * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+// <summary>
+//   Defines the AppearanceUpdate type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ZoneEngine.Packets
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     using AO.Core;
 
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
+
     using ZoneEngine.Misc;
+
+    using Identity = SmokeLounge.AOtomation.Messaging.GameData.Identity;
 
     public static class AppearanceUpdate
     {
+        #region Public Methods and Operators
+
         public static void AnnounceAppearanceUpdate(Character character)
         {
+            var message = new AppearanceUpdateMessage
+                              {
+                                  Identity =
+                                      new Identity
+                                          {
+                                              Type = IdentityType.CanbeAffected, 
+                                              Instance = character.Id
+                                          }, 
+                                  Unknown = 0x00, 
+                              };
+
             List<AOMeshs> meshs;
-            PacketWriter packetWriter = new PacketWriter();
-            packetWriter.PushByte(0xDF);
-            packetWriter.PushByte(0xDF);
-            packetWriter.PushShort(10);
-            packetWriter.PushShort(1);
-            packetWriter.PushShort(0); // Packet size. writer will take care of this
-            packetWriter.PushInt(3086);
-            packetWriter.PushInt(character.Id);
-            packetWriter.PushInt(0x41624F0D);
-            packetWriter.PushIdentity(50000, character.Id);
-            packetWriter.PushByte(0);
-            packetWriter.Push3F1Count(5); // Textures are always transferred
             int c;
 
             bool socialonly;
             bool showsocial;
+
             /*
             bool showhelmet;
             bool LeftPadVisible;
@@ -69,10 +82,10 @@ namespace ZoneEngine.Packets
             int BackMeshValue;
             int ShoulderMeshRightValue;
              */
-            //int ShoulderMeshRightValue;
-
+            // int ShoulderMeshRightValue;
             int VisualFlags;
             int PlayField;
+
             /*
             int OverrideTextureHead;
             int OverrideTextureWeaponRight;
@@ -82,16 +95,17 @@ namespace ZoneEngine.Packets
             int OverrideTextureBack;
             int OverrideTextureAttractor;
             */
-            List<AOTextures> textures = new List<AOTextures>();
+            var textures = new List<AOTextures>();
 
-            Dictionary<int, int> socialTab = new Dictionary<int, int>();
+            var socialTab = new Dictionary<int, int>();
 
             lock (character)
             {
                 VisualFlags = character.Stats.VisualFlags.Value;
 
-                socialonly = ((character.Stats.VisualFlags.Value & 0x40) > 0);
-                showsocial = ((character.Stats.VisualFlags.Value & 0x20) > 0);
+                socialonly = (character.Stats.VisualFlags.Value & 0x40) > 0;
+                showsocial = (character.Stats.VisualFlags.Value & 0x20) > 0;
+
                 /*
                 showhelmet = ((character.Stats.VisualFlags.Value & 0x4) > 0);
                 LeftPadVisible = ((character.Stats.VisualFlags.Value & 0x1) > 0);
@@ -111,7 +125,7 @@ namespace ZoneEngine.Packets
                 BackMeshValue = character.Stats.BackMesh.Value;
                 ShoulderMeshRightValue = character.Stats.ShoulderMeshRight.Value;
                  */
-                //ShoulderMeshLeftValue = character.Stats.ShoulderMeshLeft.Value;
+                // ShoulderMeshLeftValue = character.Stats.ShoulderMeshLeft.Value;
                 /*
                 OverrideTextureHead = character.Stats.OverrideTextureHead.Value;
                 OverrideTextureWeaponRight = character.Stats.OverrideTextureWeaponRight.Value;
@@ -123,12 +137,12 @@ namespace ZoneEngine.Packets
                 */
                 PlayField = character.PlayField;
 
-                foreach (int num in character.SocialTab.Keys)
+                foreach (var num in character.SocialTab.Keys)
                 {
                     socialTab.Add(num, character.SocialTab[num]);
                 }
 
-                foreach (AOTextures texture in character.Textures)
+                foreach (var texture in character.Textures)
                 {
                     textures.Add(new AOTextures(texture.place, texture.Texture));
                 }
@@ -136,7 +150,9 @@ namespace ZoneEngine.Packets
                 meshs = MeshLayers.GetMeshs(character, showsocial, socialonly);
             }
 
-            AOTextures aotemp = new AOTextures(0, 0);
+            var texturesToSend = new List<Texture>();
+
+            var aotemp = new AOTextures(0, 0);
             for (c = 0; c < 5; c++)
             {
                 aotemp.Texture = 0;
@@ -150,6 +166,7 @@ namespace ZoneEngine.Packets
                         break;
                     }
                 }
+
                 if (showsocial)
                 {
                     if (socialonly)
@@ -165,26 +182,26 @@ namespace ZoneEngine.Packets
                     }
                 }
 
-                packetWriter.PushInt(aotemp.place);
-                packetWriter.PushInt(aotemp.Texture);
-                packetWriter.PushInt(0);
+                texturesToSend.Add(new Texture { Place = aotemp.place, Id = aotemp.Texture, Unknown = 0 });
             }
 
-            c = meshs.Count;
-            packetWriter.Push3F1Count(c);
-            foreach (AOMeshs mesh in meshs)
-            {
-                packetWriter.PushByte((byte)mesh.Position);
-                packetWriter.PushUInt(mesh.Mesh);
-                packetWriter.PushInt(mesh.OverrideTexture); // Override Texture!!!!!!
-                packetWriter.PushByte((byte)mesh.Layer);
-            }
+            message.Textures = texturesToSend.ToArray();
 
-            packetWriter.PushShort((short)(VisualFlags)); // 673 = VisualFlags
-            packetWriter.PushByte(0);
+            message.Meshes =
+                meshs.Select(
+                    mesh =>
+                    new Mesh
+                        {
+                            Position = (byte)mesh.Position, 
+                            Id = (uint)mesh.Mesh, 
+                            OverrideTextureId = mesh.OverrideTexture, 
+                            Layer = (byte)mesh.Layer
+                        }).ToArray();
+            message.VisualFlags = (short)VisualFlags;
 
-            byte[] reply = packetWriter.Finish();
-            Announce.Playfield(PlayField, reply);
+            Announce.Playfield(PlayField, message);
         }
+
+        #endregion
     }
 }
