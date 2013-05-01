e70bd86a1b4572d95f4e00d9b3356c4fb825d312
bitterskittles <basementk1tt3h@gmail.com>
2013-05-01 17:01:20 +0100

    Effect now constructs SpellListMessage.

--- CellAO/AO.Servers/ZoneEngine/ChatCommands/Effect.cs
+++ CellAO/AO.Servers/ZoneEngine/ChatCommands/Effect.cs
@@ -1,82 +1,56 @@
-﻿#region License
-// Copyright (c) 2005-2012, CellAO Team
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
-//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
-
-#region Usings...
-#endregion
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Effect.cs" company="CellAO Team">
+//   Copyright © 2005-2013 CellAO Team.
+//   
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//   
+//       * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//       * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//       * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+// <summary>
+//   Defines the ChatCommandEffect type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ZoneEngine.ChatCommands
 {
     using System;
     using System.Collections.Generic;
 
-    using AO.Core;
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
 
     using ZoneEngine.Misc;
     using ZoneEngine.Script;
 
+    using Identity = AO.Core.Identity;
+
     public class ChatCommandEffect : AOChatCommand
     {
-        public override void ExecuteCommand(Client client, Identity target, string[] args)
-        {
-            // No check needed, its done by CheckCommandArguments
-            int gfx = int.Parse(args[3]);
+        #region Public Methods and Operators
 
-            //begin assembling packet here
-            PacketWriter packet = new PacketWriter();
-            packet.PushByte(0xDF);
-            packet.PushByte(0xDF);
-            packet.PushShort(10);
-            packet.PushShort(1);
-            packet.PushShort(0);
-            packet.PushInt(3086);
-            packet.PushInt(client.Character.Id);
-            packet.PushInt(0x4D450114);
-            packet.PushIdentity(target);
-            packet.PushByte(0);
-            packet.Push3F1Count(1); // effects count
-            // effect starts
-            packet.PushIdentity(53030, 0); // effect ID (53030 = GfxEffect)
-            packet.PushInt(4); // ?
-            packet.PushInt(0); // Criterion count
-            packet.PushInt(1); // Hits
-            packet.PushInt(0); // Delay
-            packet.PushInt(0); // 
-            packet.PushInt(0); // 
-            // effect args
-            packet.PushInt(gfx); // Value
-            packet.PushInt(0); // GfxLife
-            packet.PushInt(0); // GfxSize
-            packet.PushInt(0); // GfxRed
-            packet.PushInt(0); // GfxGreen
-            packet.PushInt(0); // GfxBlue
-            packet.PushInt(0); // GfxFade
-            // effect args end
-            // effect ends
-            packet.PushIdentity(50000, client.Character.Id);
-            byte[] reply = packet.Finish();
-            //done creating the packet
-            Announce.Playfield(client.Character.PlayField, reply);
+        public override bool CheckCommandArguments(string[] args)
+        {
+            var check = new List<Type>();
+            check.Add(typeof(string));
+            check.Add(typeof(string));
+            check.Add(typeof(int));
+            return CheckArgumentHelper(check, args);
         }
 
         public override void CommandHelp(Client client)
@@ -84,13 +58,67 @@ namespace ZoneEngine.ChatCommands
             client.SendChatText("/command effect gfxeffect value <int>");
         }
 
-        public override bool CheckCommandArguments(string[] args)
+        public override void ExecuteCommand(Client client, Identity target, string[] args)
         {
-            List<Type> check = new List<Type>();
-            check.Add(typeof(string));
-            check.Add(typeof(string));
-            check.Add(typeof(int));
-            return CheckArgumentHelper(check, args);
+            // No check needed, its done by CheckCommandArguments
+            var gfx = int.Parse(args[3]);
+
+            var nanoEffect = new NanoEffect
+                                 {
+                                     Effect =
+                                         new SmokeLounge.AOtomation.Messaging.GameData.Identity
+                                             {
+                                                 Type =
+                                                     IdentityType
+                                                     .GfxEffect, 
+                                                 Instance
+                                                     = 0
+                                             }, 
+                                     Unknown1 = 0x00000004, 
+                                     CriterionCount = 0x00000000, 
+                                     Hits = 0x00000001, 
+                                     Delay = 0x00000000, 
+                                     Unknown2 = 0x00000000, 
+                                     Unknown3 = 0x00000000, 
+                                     GfxValue = gfx, 
+                                     GfxLife = 0x00000000, 
+                                     GfxSize = 0x00000000, 
+                                     GfxRed = 0x00000000, 
+                                     GfxGreen = 0x00000000, 
+                                     GfxBlue = 0x00000000, 
+                                     GfxFade = 0x00000000
+                                 };
+            var msg = new SpellListMessage
+                          {
+                              Identity =
+                                  new SmokeLounge.AOtomation.Messaging.GameData.Identity
+                                      {
+                                          Type =
+                                              (
+                                              IdentityType
+                                              )
+                                              target
+                                                  .Type, 
+                                          Instance =
+                                              target
+                                              .Instance
+                                      }, 
+                              Unknown = 0x00, 
+                              NanoEffects = new[] { nanoEffect }, 
+                              Character =
+                                  new SmokeLounge.AOtomation.Messaging.GameData.Identity
+                                      {
+                                          Type =
+                                              IdentityType
+                                              .CanbeAffected, 
+                                          Instance =
+                                              client
+                                              .Character
+                                              .Id
+                                      }, 
+                          };
+
+            Announce.Playfield(client.Character.PlayField, msg);
         }
 
         public override int GMLevelNeeded()
@@ -101,9 +129,11 @@ namespace ZoneEngine.ChatCommands
 
         public override List<string> ListCommands()
         {
-            List<string> temp = new List<string>();
+            var temp = new List<string>();
             temp.Add("effect");
             return temp;
         }
+
+        #endregion
     }
 }
