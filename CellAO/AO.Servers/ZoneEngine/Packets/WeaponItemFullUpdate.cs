440e84bf4714bb38565959ab9a4cee2758aba426
bitterskittles <basementk1tt3h@gmail.com>
2013-05-01 16:30:44 +0100

    WeaponItemFullUpdate now constructs WeaponItemFullUpdateMessage.

--- CellAO/AO.Servers/ZoneEngine/Packets/WeaponItemFullUpdate.cs
+++ CellAO/AO.Servers/ZoneEngine/Packets/WeaponItemFullUpdate.cs
@@ -1,36 +1,41 @@
-﻿#region License
-// Copyright (c) 2005-2012, CellAO Team
-// 
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
-//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//     * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#endregion
-
-#region Usings...
-#endregion
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WeaponItemFullUpdate.cs" company="CellAO Team">
+//   Copyright © 2005-2013 CellAO Team.
+//   
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//   
+//       * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//       * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//       * Neither the name of the CellAO Team nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+// <summary>
+//   Defines the WeaponItemFullUpdate type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ZoneEngine.Packets
 {
-    using AO.Core;
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
 
     public static class WeaponItemFullUpdate
     {
+        #region Public Methods and Operators
+
         public static void SendOwner(Client client, Character character)
         {
             // ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
@@ -39,56 +44,92 @@ namespace ZoneEngine.Packets
 
             // notes: one packet has to be sent for each hand!!!!!!! (more for each weapon, see below)
             // Packet only sent when Weapons are in Posession or being equipped the first time
+            var message = new WeaponItemFullUpdateMessage
+                              {
+                                  Identity = new Identity
+                                                 {
+                                                     // Type Weapon (always instanced)
+                                                     Type =
+                                                         IdentityType.WeaponInstance, 
 
-            PacketWriter packetWriter = new PacketWriter();
+                                                     // Instance of Weapon, hardcoding this causes a crash if you want to equip more than one weapon
+                                                     Instance = 0x45AEE789
+                                                 }, 
+                                  Unknown = 0x00, 
+                                  Unknown1 = 0x0000000B, 
+                                  Character = new Identity
+                                                  {
+                                                      // Reciever
+                                                      Type =
+                                                          (IdentityType)
+                                                          character.Type, 
+                                                      Instance = character.Id
+                                                  }, 
+                                  Unknown2 = 0x00000C0A, 
+                                  
+                                  
+                                  
+                                  // constant
+                                  Unknown3 = 0x000F424F, 
+                                  Unknown4 = 0x0000, 
 
-            packetWriter.PushByte(0xDF); // starter
-            packetWriter.PushByte(0xDF); // starter
-            packetWriter.PushShort(0xA);
-            packetWriter.PushShort(0x1);
-            packetWriter.PushShort(0); // Length
-            packetWriter.PushInt(3086); // Sender
-            packetWriter.PushInt(character.Id); // Reciever
-            packetWriter.PushInt(0x3B1D2268); // Packet ID
-            packetWriter.PushInt(0xC74A); // Type Weapon (always instanced)
-            packetWriter.PushInt(0x45AEE789);
-            // Instance of Weapon, hardcoding this causes a crash if you want to equip more than one weapon
-            packetWriter.PushByte(0);
-            packetWriter.PushInt(0xB);
-            packetWriter.PushInt(character.Type);
-            packetWriter.PushInt(character.Id); // Reciever
-            packetWriter.PushInt(0xC0A);
-            packetWriter.PushInt(0xF424F); // constant
-            packetWriter.PushShort(0);
-            packetWriter.PushInt(0x6); // Placement/Location Maybe?
-            packetWriter.PushInt(0x1F88); // Another Constant
+                                  // Placement/Location Maybe?
+                                  Unknown5 = 0x00000006, 
 
-            // Stat 1
-            packetWriter.PushInt(0); // constant Flags
-            packetWriter.PushUInt(0x403); // Item Flag
-            // Stat 2
-            packetWriter.PushInt(0x17); // constant Static Instance?
-            packetWriter.PushInt(0x1e6d0); // Weapon ITEM ID
-            // Stat 3
-            packetWriter.PushInt(0x2BD); //constant ACG Item Level
-            packetWriter.PushInt(0x5); // Quality-Level
-            // Stat 4
-            packetWriter.PushInt(0x2BE); // constant ACGItemTemplateID
-            packetWriter.PushInt(0x1e6d0); // Weapon ITEM ID (low ID)
-            // Stat 5
-            packetWriter.PushInt(0x2BF); // constant ACGItemTemplateID2
-            packetWriter.PushInt(0x1e6d1); // High ID
-            // Stat 6
-            packetWriter.PushInt(0x19C); // Constant MultipleCount
-            packetWriter.PushInt(0x1); // Amount
-            // Stat 7
-            packetWriter.PushInt(0x1A); // Constant Energy
-            packetWriter.PushUInt(0x28); // Ammo
-            // End
-            packetWriter.PushInt(0); // Empty fill
+                                  // Another Constant
+                                  Unknown6 = 0x00001F88, 
 
-            byte[] packet = packetWriter.Finish();
-            client.SendCompressed(packet);
+                                  // constant Flags
+                                  Flags = 0x00000000, 
+                                  
+                                  
+                                  
+                                  // Item Flag
+                                  ItemFlags = 0x00000403, 
+
+                                  // constant Static Instance?
+                                  StaticInstance = 0x00000017, 
+
+                                  // Weapon ITEM ID
+                                  WeaponItemId = 0x0001E6D0, 
+                                  
+                                  
+                                  
+                                  // constant ACG Item Level
+                                  AcgItemLevel = 0x000002BD, 
+
+                                  // Quality-Level
+                                  QualityLevel = 0x00000005, 
+
+                                  // constant ACGItemTemplateID
+                                  AcgItemTemplateId = 0x000002BE, 
+
+                                  // Weapon ITEM ID (low ID)
+                                  WeaponItemLowId = 0x0001E6D0, 
+
+                                  // constant ACGItemTemplateID2
+                                  AcgItemTemplateId2 = 0x000002BF, 
+
+                                  // High ID
+                                  WeaponItemHighId = 0x0001E6D1, 
+
+                                  // Constant MultipleCount
+                                  MultipleCount = 0x0000019C, 
+
+                                  // Amount
+                                  Amount = 0x00000001, 
+
+                                  // Constant Energy
+                                  Energy = 0x0000001A, 
+
+                                  // Ammo
+                                  Ammo = 0x00000028, 
+
+                                  // Empty fill
+                                  Unknown7 = 0x00000000
+                              };
+
+            client.SendCompressed(message);
         }
 
         public static void SendPlayfield(Client client, Character character)
@@ -99,55 +140,91 @@ namespace ZoneEngine.Packets
 
             // notes: one packet has to be sent for each hand!!!!!!!
             // Packet only sent when Weapons are in Posession!
+            var message = new WeaponItemFullUpdateMessage
+                              {
+                                  Identity = new Identity
+                                                 {
+                                                     // Type Weapon (always instanced)
+                                                     Type =
+                                                         IdentityType.WeaponInstance, 
 
-            PacketWriter packetWriter = new PacketWriter();
+                                                     // Instance of Weapon, hardcoding this causes a crash if you want to equip more than one weapon
+                                                     Instance = 0x45AEE789
+                                                 }, 
+                                  Unknown = 0x00, 
+                                  Unknown1 = 0x0000000B, 
+                                  Character = new Identity
+                                                  {
+                                                      // Reciever
+                                                      Type =
+                                                          (IdentityType)
+                                                          character.Type, 
+                                                      Instance = character.Id
+                                                  }, 
+                                  Unknown2 = 0x00000C0A, 
 
-            packetWriter.PushByte(0xDF); // starter
-            packetWriter.PushByte(0xDF); // starter
-            packetWriter.PushShort(0xA);
-            packetWriter.PushShort(0x1);
-            packetWriter.PushShort(0); // Length
-            packetWriter.PushInt(3086); // Sender
-            packetWriter.PushInt(character.Id); // Reciever
-            packetWriter.PushInt(0x3B1D2268); // Packet ID
-            packetWriter.PushInt(0xC74A); // .... Weapon maybe?
-            packetWriter.PushInt(0x45AEE789); // ID
-            packetWriter.PushByte(0);
-            packetWriter.PushInt(0xB);
-            packetWriter.PushInt(0xC350);
-            packetWriter.PushInt(character.Id); // Reciever
-            packetWriter.PushInt(0xC0A);
-            packetWriter.PushInt(0xF424F); // constant
-            packetWriter.PushShort(0);
-            packetWriter.PushInt(0x6); // Placement/Location Maybe?
-            packetWriter.PushInt(0x1F88); // Another Constant
 
-            // Stat 1
-            packetWriter.PushInt(0); // constant Flags
-            packetWriter.PushUInt(0x403); // Item Flag
-            // Stat 2
-            packetWriter.PushInt(0x17); // constant Static Instance?
-            packetWriter.PushInt(0x1e6d0); // Weapon ITEM ID
-            // Stat 3
-            packetWriter.PushInt(0x2BD); //constant ACG Item Level
-            packetWriter.PushInt(0x5); // Quality-Level
-            // Stat 4
-            packetWriter.PushInt(0x2BE); // constant ACGItemTemplateID
-            packetWriter.PushInt(0x1e6d0); // Weapon ITEM ID (low ID)
-            // Stat 5
-            packetWriter.PushInt(0x2BF); // constant ACGItemTemplateID2
-            packetWriter.PushInt(0x1e6d1); // High ID
-            // Stat 6
-            packetWriter.PushInt(0x19C); // Constant MultipleCount
-            packetWriter.PushInt(0x1); // Amount
-            // Stat 7
-            packetWriter.PushInt(0x1A); // Constant Energy
-            packetWriter.PushUInt(0x28); // Ammo
-            // End
-            packetWriter.PushInt(0); // Empty fill
+                                  // constant
+                                  Unknown3 = 0x000F424F, 
+                                  Unknown4 = 0x0000, 
 
-            byte[] packet = packetWriter.Finish();
-            client.SendCompressed(packet);
+                                  // Placement/Location Maybe?
+                                  Unknown5 = 0x00000006, 
+
+                                  // Another Constant
+                                  Unknown6 = 0x00001F88, 
+
+                                  // constant Flags
+                                  Flags = 0x00000000, 
+
+
+                                  // Item Flag
+                                  ItemFlags = 0x00000403, 
+
+                                  // constant Static Instance?
+                                  StaticInstance = 0x00000017, 
+
+                                  // Weapon ITEM ID
+                                  WeaponItemId = 0x0001E6D0, 
+
+
+                                  // constant ACG Item Level
+                                  AcgItemLevel = 0x000002BD, 
+
+                                  // Quality-Level
+                                  QualityLevel = 0x00000005, 
+
+                                  // constant ACGItemTemplateID
+                                  AcgItemTemplateId = 0x000002BE, 
+
+                                  // Weapon ITEM ID (low ID)
+                                  WeaponItemLowId = 0x0001E6D0, 
+
+                                  // constant ACGItemTemplateID2
+                                  AcgItemTemplateId2 = 0x000002BF, 
+
+                                  // High ID
+                                  WeaponItemHighId = 0x0001E6D1, 
+
+                                  // Constant MultipleCount
+                                  MultipleCount = 0x0000019C, 
+
+                                  // Amount
+                                  Amount = 0x00000001, 
+
+                                  // Constant Energy
+                                  Energy = 0x0000001A, 
+
+                                  // Ammo
+                                  Ammo = 0x00000028, 
+
+                                  // Empty fill
+                                  Unknown7 = 0x00000000
+                              };
+
+            client.SendCompressed(message);
         }
+
+        #endregion
     }
 }
